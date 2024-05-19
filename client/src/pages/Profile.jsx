8d85f4df524e43df92598e56408c3bfb187473d5
vinzz34 <vinaygn29@gmail.com>
2024-05-19 12:08:19 +0530

    added image upload functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,21 +2,58 @@ import { useSelector } from "react-redux"
 import {useForm} from "react-hook-form"
 import validator from "validator"
 import instance from "../api/api_instance"
+import { useEffect, useRef, useState } from "react"
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from "firebase/storage"
+import {getApp} from "firebase/app"
 
 const Profile = () => {
 
   const {currentUser} = useSelector((state) => state.user)
 
+  const fileRef = useRef()
+
+  const [file,setFile] = useState()
+  const [filePerc,setFilePerc] = useState(0)
+  const [fileError,setFileError] = useState(false)
+
   const {
     register,
     handleSubmit,
     setError,
+    watch,
+    setValue,
     formState : {errors,isSubmitting}} = useForm({
       defaultValues: {
         username: currentUser.username,
         email: currentUser.email
       }
     })
+
+  useEffect(() => {
+    const handleFileUpload = async (file) => {
+      const app =getApp()
+      const storage = getStorage(app)
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage,fileName)
+      const uploadTask = uploadBytesResumable(storageRef,file)
+      uploadTask.on('state_changed',(snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        setFilePerc(Math.round(progress))
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setFileError(true)
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setValue("avatar",downloadURL)
+        })
+      })
+    }
+    if(file){
+      handleFileUpload(file)
+    }
+  },[file,setValue])
 
   const onSubmit = async (data) => {
     console.log(data)
@@ -26,7 +63,16 @@ const Profile = () => {
     <div className="p-3 max-w-lg mx-auto">
       <h2 className="text-3xl font-semibold text-center my-7">Profile</h2>
       <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
-        <img className="w-24 h-24 rounded-full self-center" src={currentUser.avatar} alt="avatar" />
+        <input onChange={(e) => setFile(e.target.files[0])} type="file" accept="image/*" hidden ref={fileRef} />
+        <img onClick={() => fileRef.current.click()} className="w-24 h-24 rounded-full self-center cursor-pointer" src={watch("avatar") || currentUser.avatar} alt="avatar" />
+
+        {fileError ? (
+          <div className="text-red-700 text-sm text-center">Error Image upload (image must be less than 2 mb)</div>
+        ) : filePerc > 0 && filePerc < 100 ? (
+          <div className="text-slate-700 text-sm text-center">{`Uploading ${filePerc}%`}</div>
+        ) : filePerc === 100 && (
+          <div className="text-green-700 text-sm text-center">Image successfully uploaded!</div>
+        )}
 
         <input {...register("username",{
           required: "Username is required"
