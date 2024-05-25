1e79d6a7f3609012257d8ce78f4d9f2a9d796576
vinzz34 <vinaygn29@gmail.com>
2024-05-25 22:25:42 +0530

    created update listing api route

--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { createListing,deleteListing } from "../controllers/listing.controller.js"
+import { createListing,deleteListing,updateListing } from "../controllers/listing.controller.js"
 import { verifyUser } from "../middleware/verifyUser.js"
 
 const router = express.Router()
@@ -7,5 +7,6 @@ const router = express.Router()
 router.use(verifyUser)
 router.post('/create',createListing)
 router.delete('/delete/:id',deleteListing)
+router.patch('/update/:id',updateListing)
 
 export default router
