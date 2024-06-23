27811112a143c021029e8bb16688a407dd2cf430
vinzz34 <vinaygn29@gmail.com>
2024-06-23 22:10:40 +0530

    changed BrowserRouter to HashRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route,HashRouter} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
@@ -12,7 +12,7 @@ import Listing from "./pages/Listing";
 import Search from "./pages/Search";
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -27,7 +27,7 @@ const App = () => {
           <Route path="/update-listing/:id" element={<UpdateListing />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -7,7 +7,6 @@ import userRouter from "./routes/user.route.js"
 import listingRouter from "./routes/listing.route.js"
 import error from "./middleware/error.js"
 import cookieParser from "cookie-parser"
-import path from "path"
 
 dotenv.config()
 
@@ -23,7 +22,6 @@ mongoose.connect(process.env.MONGO)
         console.log(error)
     })
 
-const __dirname = path.resolve()
 
 app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
 
@@ -33,11 +31,5 @@ app.use(cookieParser())
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/listing',listingRouter)
-
-app.use(express.static(path.join(__dirname, '/client/dist')))
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/dist/index.html'))
-})
 
 app.use(error)
