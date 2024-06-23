c1bccc917757b2b21eb32031ad58fe5ea112710d
vinzz34 <vinaygn29@gmail.com>
2024-06-23 22:02:26 +0530

    fixed 404 error on refresh

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/user.route.js"
 import listingRouter from "./routes/listing.route.js"
 import error from "./middleware/error.js"
 import cookieParser from "cookie-parser"
+import path from "path"
 
 dotenv.config()
 
@@ -22,6 +23,8 @@ mongoose.connect(process.env.MONGO)
         console.log(error)
     })
 
+const __dirname = path.resolve()
+
 app.use(cors({credentials: true, origin: process.env.CLIENT_URL}));
 
 app.use(express.json())
@@ -30,5 +33,11 @@ app.use(cookieParser())
 app.use('/api/auth',authRouter)
 app.use('/api/user',userRouter)
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/dist/index.html'))
+})
 
 app.use(error)
